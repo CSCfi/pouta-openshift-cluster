df56665369c17a7ca9f5eac025462411ef654651
Apurva Nandan <apurva3000@gmail.com>
2016-10-13 08:09:05 +0000

    jupyter notebook support for hortonworks cluster

---
+++ roles/jupyter_notebook/files/jupyter.sh
@@ -0,0 +1,6 @@
+source /etc/profile.d/virtualenvwrapper.sh
+mkvirtualenv jupyter --system-site-packages
+workon jupyter
+pip install --upgrade setuptools pip
+pip install jupyter
+pip install findspark
