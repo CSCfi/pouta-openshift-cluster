8b13bd2529dc845aad74ee8d25a874856404b598
Olli Tourunen <olli.tourunen@csc.fi>
2017-10-13 13:16:56 +0300

    support scaling masters and etcd
    This patch adds support for running openshift-ansible's scaleup -playbooks
    on master and etcd hosts. This is especially useful for single node recovery.
    
    Changes:
    - refactor install.yml to plain installation and scaleup cases
    - split getting and setting previous installation state to separate
      files.
    - make installation condition trigger on new_masters group size
    - added proof of concept scripts for rolling reinstallation and
      external load balancer ip failover.
    
    Fixes:
    - post_install: registry volume is now reassigned if needed. Previously it was
      only attached if it did not exist beforehand

---
+++ scripts/select_lb.bash
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# Proof of concept script to check that OpenStack API responds and
+# assign floating ip to another load balancer in case it does not.
+# It assumes that there are two load balancers called
+# ${ENV_NAME}-lb-{1,2}
+#
+# NOTE: This code has been used for developing scaleup playbooks.
+#       It has not been tested in other scenarios. Use with caution.
+#
+
+
+set -e
+
+date
+
+if ! curl -s -k -o /dev/null --connect-timeout 5 https://$PUBLIC_IP:8443; then
+
+    current_lb=$(openstack server list -c Name -c Networks | grep $PUBLIC_IP | sed -e "s/ //g" | cut -d "|" -f 2)
+    echo "Currently active LB: '$current_lb'"
+    current_idx=${current_lb:(-1)}
+    new_idx=$(( current_idx%2 + 1 ))
+    new_host=${ENV_NAME}-lb-$new_idx
+
+    echo "curl failed, re-assigning floating ip to $new_host"
+    openstack server add floating ip $new_host $PUBLIC_IP
+fi
