95006583d8f0c918e993f3e590b351686e82f7bd
Olli Tourunen <olli.tourunen@csc.fi>
2017-12-12 14:16:44 +0200

    add periodic object pruning
    Run pruning for old OpenShift objects from cron on masters. Pruning
    is done on all masters at different times for redundancy.

---
+++ playbooks/roles/openshift_master/files/poc_prune_objects.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Script to prune old objects from OpenShift.
+#
+# Ref: https://docs.openshift.org/3.6/admin_guide/pruning_resources.html
+
+export PATH="${PATH}:/usr/local/bin/"
+
+for object in builds deployments; do
+    echo "pruning $object"
+    oadm prune $object --orphans --keep-complete=5 --keep-failed=1 --keep-younger-than=168h --confirm
+done
