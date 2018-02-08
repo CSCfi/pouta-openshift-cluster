fc1c0405eb896a5802cc6f0e7a407e90fb7802fd
tourunen <olli.tourunen@csc.fi>
2018-02-08 13:36:44 +0200

    dynamic selection of openshift-ansible version
    To be able to deploy clusters with different versions by the same code
    base, we move checking out openshift-ansible from the host environment
    into the deployment container. The version is selected based on
    variables set in openshift-inventory for the environment in question.
    
    Changes:
    - remove checking out openshift-environment in gitlab CI pipeline
    - export deployment version data in poc-deployer role
    - init_env.bash
        - source deployment version data
        - check out given branch of openshift-ansible from given repo,
          unless ~/openshift-ansible directory already exists
        - change single brackets to double brackets
        - improve output and readability
    - move backup playbook selection logic from gitlab CI pipeline to
      deployment container (run_backup.bash)
    - add a command line option to mount openshift-ansible from the host
      to run_deployment_container.bash

--- container-src/poc-deployer/init_env.bash
+++ container-src/poc-deployer/init_env.bash
@@ -7,7 +7,7 @@ env_name=$ENV_NAME
 echo "Initializing environment for $env_name"
 echo
 
-if [ ! -e /dev/shm/secret/vaultpass ]; then
+if [[ ! -e /dev/shm/secret/vaultpass ]]; then
     mkdir -p /dev/shm/secret/
     touch /dev/shm/secret/vaultpass
     chmod 600 /dev/shm/secret/vaultpass
@@ -27,6 +27,7 @@ echo
 export ANSIBLE_LIBRARY="/usr/share/ansible:\
 $HOME/openshift-ansible/roles/lib_utils/library:\
 $HOME/openshift-ansible/roles/lib_openshift/library"
+
 echo "ANSIBLE_LIBRARY set to $ANSIBLE_LIBRARY"
 
 pushd /opt/deployment/poc/playbooks > /dev/null
@@ -38,9 +39,15 @@ ansible-galaxy install -f -p $HOME/galaxy-roles -r requirements.yml
 echo "Initializing ramdisk contents"
 echo
 SKIP_DYNAMIC_INVENTORY=1 ansible-playbook initialize_ramdisk.yml
+echo
+
+echo "Sourcing OpenStack credentials"
 source /dev/shm/$env_name/openrc.sh
 
-if [ "$SKIP_SSH_CONFIG" == "1" ]; then
+echo "Sourcing deployment data"
+source /dev/shm/$env_name/deployment_data.sh
+
+if [[ "$SKIP_SSH_CONFIG" == "1" ]]; then
     echo
     echo "Skipping ssh config generation"
     echo
@@ -49,6 +56,19 @@ else
     echo "Generating ssh config entries"
     echo
     ansible-playbook generate_ssh_config.yml
+fi
+
+if [[ -e $HOME/openshift-ansible ]]; then
+    echo
+    echo "Using mounted openshift-ansible"
+    echo
+else
+    echo
+    echo "Checking out openshift-ansible"
+    echo "    Repo: $OPENSHIFT_ANSIBLE_REPO"
+    echo "  Branch: $OPENSHIFT_ANSIBLE_BRANCH"
+    echo
+    git clone --depth=1 -b $OPENSHIFT_ANSIBLE_BRANCH $OPENSHIFT_ANSIBLE_REPO $HOME/openshift-ansible
 fi
 
 popd > /dev/null
