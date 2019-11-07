bf8db504dae274ddcefb8d14595c7b04edccec5e
Yacine Khettab <yacine.khettab@csc.fi>
2019-11-07 13:10:34 +0200

    dcterm.bash enhancements
    Improve dctem.bash in order to be able to launch a deployment
    container for any environment without having to type sudo nor
    "POC_ENV_NAME".
    
    This script can be added to the user environment and it will
    allow to run the deployment container from anywhere using a
    simpler command: `dct <env_name>` by running:
    
    ```
    sudo ln -s /home/ykhettab/git/poc/scripts/dcterm.bash \
               /usr/local/bin/dct
    ```

--- scripts/dcterm.bash
+++ scripts/dcterm.bash
@@ -4,24 +4,28 @@
 # This script acts as a wrapper around run_deployment_container, and opens
 # a session for an environment either in a new or existing deployment container.
 # The name of the environment is extracted from active tmux session name or from
-# an environment variable 'POC_ENV_NAME'.
+# the first shell arg.
+#
+
+# get sudo rights if we do not have them already
+[ "$UID" -eq 0 ] || exec sudo "$0" "$@"
 
 # figure out where we live to find run_deployment_container.bash
-script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+script_dir="$(dirname "$(readlink -f "$0")")"
 
 # try to extract session name from tmux
 if [[ ! -z $TMUX ]]; then
     env_name=$(tmux display-message -p '#S')
 fi
 
-# fallback: extract environment name from POC_ENV_NAME
+# fallback: extract environment name from args
 if [[ -z $env_name ]]; then
-    env_name=$POC_ENV_NAME
+    env_name=$1
 fi
 
 # check if we have the environment name figured out
 if [[ -z $env_name ]]; then
-    echo "unable to extract environment name from tmux or POC_ENV_NAME"
+    echo "unable to extract environment name from tmux or shell arguments"
     exit 1
 fi
 
@@ -42,5 +46,5 @@ else
     fi
     echo
 
-    ${script_dir}/run_deployment_container.bash -p $vaultpass_path -e $env_name $*
+    ${script_dir}/run_deployment_container.bash -p $vaultpass_path -e $env_name
 fi
