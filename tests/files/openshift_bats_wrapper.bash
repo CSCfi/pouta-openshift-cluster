c24973d42980f64e8f6c0b380569ae8d8c5cf8dc
Teemu Frisk <teemu.frisk@csc.fi>
2020-07-21 10:58:16 +0300

    Fix test scripts based on shellcheck and ansible-lint checks

--- tests/files/openshift_bats_wrapper.bash
+++ tests/files/openshift_bats_wrapper.bash
@@ -1,22 +1,25 @@
 #!/usr/bin/env bash
 
-num_failed=0
-for test_dir in $*; do
-    export project_name="system-tests-$(date '+%Y%m%d%H%M%S')-$(openssl rand -hex 6)"
-    echo "Running test in $test_dir, Using project $project_name"
-    oc new-project $project_name 2>&1 > /dev/null
+set -eu
 
-    cd $test_dir
+num_failed=0
+for test_dir in "$@"; do
+    project_name="system-tests-$(date '+%Y%m%d%H%M%S')-$(openssl rand -hex 6)"
+    export project_name
+    echo "Running test in $test_dir, Using project $project_name"
+    oc new-project "$project_name" > /dev/null 2>&1
+
+    cd "$test_dir" || exit
     ./run.bats
     result=$?
-    cd ..
+    cd .. || exit
 
     oc project default 2>&1
-    oc delete project $project_name
+    oc delete project "$project_name"
 
     if [ "$result" -ne 0 ]; then
       ((num_failed++));
     fi
 done
 
-exit $num_failed
+exit "$num_failed"
