ded478e225c06a7fd33c0b1f9fb93aea7cfb7ab5
Vartika Shah <varshah@berguv.lnx.csc.fi>
2023-06-26 14:32:46 +0300

    update the docker image reppo for poc-deployer

--- scripts/run_deployment_container.bash
+++ scripts/run_deployment_container.bash
@@ -30,7 +30,7 @@ print_usage_and_exit()
 }
 
 docker_opts='-it'
-container_image='docker-registry.rahti.csc.fi/rahti-docker-prod/poc-deployer:1.2'
+container_image='docker-registry.rahti.csc.fi/rahti-docker-prod/poc-deployer:1.3'
 
 while getopts "p:P:e:o:c:sh" opt; do
     case $opt in
